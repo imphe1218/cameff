e8543863b5d914157e362e02cc693075ae087862
jasongaraygaykhaw <jason.garaygay.khaw@gmail.com>
2026-07-13 00:11:44 +0800

    feature/milestone2: add specialized tectonic and cascade experts

--- include/cameff/cameff.h
+++ include/cameff/cameff.h
@@ -12,5 +12,8 @@
 #include "cameff/region.h"
 #include "cameff/signals.h"
 #include "cameff/types.h"
+#include "cameff/cascade_expert.h"
+#include "cameff/crustal_fault_expert.h"
+#include "cameff/subduction_expert.h"
 
 #endif
---
+++ include/cameff/crustal_fault_expert.h
@@ -0,0 +1,8 @@
+#ifndef CAMEFF_CRUSTAL_FAULT_EXPERT_H
+#define CAMEFF_CRUSTAL_FAULT_EXPERT_H
+
+#include "cameff/expert.h"
+
+const cameff_expert_t *cameff_crustal_fault_expert(void);
+
+#endif
