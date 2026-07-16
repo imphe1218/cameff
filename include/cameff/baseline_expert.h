1fa76beea8fc7c2af29e6ef4c7cc6530382e6e13
jasongaraygaykhaw <jason.garaygay.khaw@gmail.com>
2026-07-16 23:18:05 +0800

    Major revamp. Deleting all files in this repo

--- include/cameff/baseline_expert.h
+++
@@ -1,8 +0,0 @@
-#ifndef CAMEFF_BASELINE_EXPERT_H
-#define CAMEFF_BASELINE_EXPERT_H
-
-#include "cameff/expert.h"
-
-const cameff_expert_t *cameff_baseline_expert(void);
-
-#endif
--- include/cameff/cascade_expert.h
+++
@@ -1,8 +0,0 @@
-#ifndef CAMEFF_CASCADE_EXPERT_H
-#define CAMEFF_CASCADE_EXPERT_H
-
-#include "cameff/expert.h"
-
-const cameff_expert_t *cameff_cascade_expert(void);
-
-#endif
